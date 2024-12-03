11493d8c61c7e09e8f9d088b85bb29e10cb44541
mahmoudGalal67 <mahmoudfarag270@gmail.com>
2024-12-03 11:36:55 +0200

    first commit

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,7 +3,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
+  apiKey: "AIzaSyBgpn7g_mGvP6hqiMS8dW_Mjv__e0yHqBE",
   authDomain: "chatapp-f66cc.firebaseapp.com",
   projectId: "chatapp-f66cc",
   storageBucket: "chatapp-f66cc.firebasestorage.app",
